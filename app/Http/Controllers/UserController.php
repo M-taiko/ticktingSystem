5f76dc07acdfe49c6631575237285ca20e59de9b
Mohamed tarek <M-taiko>
2023-05-07 13:45:11 +0300

    Role update fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -111,7 +111,7 @@ public function update(Request $request, $id)
         $user = User::find($id);
         $user->update($input);
         DB::table('model_has_roles')->where('model_id',$id)->delete();
-        $user->assignRole($request->input('roles'));
+        $user->syncRoles($request->input('UserRole'));
         return redirect()->route('users.index')
         ->with('edit','User Udated Sucessfully');
 }
